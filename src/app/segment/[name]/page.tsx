6ea6931573514b7c3afd9d760b8d7a97b34a8353
AlehandroSV <alehandrosantos99@gmail.com>
2025-06-23 22:43:53 -0300

    feat: refatorar a página de segmento para usar parâmetros tipados

--- src/app/segment/[name]/page.tsx
+++ src/app/segment/[name]/page.tsx
@@ -1,4 +1,12 @@
-export function SegmentPage() {
+type Params = Promise<{ city: string }>;
+type SearchParams = Promise<{ id: string }>;
+
+export default function Page(props: {
+  params: Params;
+  searchParams: SearchParams;
+}) {
+  console.log(props);
+
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-2xl font-bold text-red-600">
