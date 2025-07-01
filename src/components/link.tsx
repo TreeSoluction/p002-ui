9ab09fb77889180571a61ac068126b078aff0cb8
AlehandroSV <alehandrosantos99@gmail.com>
2025-07-01 00:13:30 -0300

    feat: substituir hooks de navegação por acesso direto ao objeto window para obter o pathname e searchParams

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import NextLink from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
 import NProgress from "nprogress";
 import { ComponentProps, MouseEvent, forwardRef } from "react";
 
@@ -9,18 +8,22 @@ type LinkProps = ComponentProps<typeof NextLink>;
 
 const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
   const { onClick, href, ...rest } = props;
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  const currentFullPath = `${pathname}${searchParams.toString() ? "?" + searchParams.toString() : ""}`;
 
   const handleClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const pathname = window.location.pathname;
+    const searchParams = window.location.search;
+
+    const currentFullPath = `${pathname}${searchParams}`;
+
     let targetHref: string;
 
     if (typeof href === "string") {
       targetHref = href;
     } else if (typeof href === "object") {
-      // reconstruir a URL com pathname + query
       const query = href.query
         ? "?" +
           Object.entries(href.query)
